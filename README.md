1f4307bb7a05966a91eeaed7dca0af249a4be008
Teodor Ande Elstad <teodor@andeelstad.com>
2015-11-14 16:39:01 +0100

    Add socket and note-grid

--- README.md
+++ README.md
@@ -5,7 +5,9 @@ An experiment playing linux synth sounds from a web server.
 
 Setup:
 * Get nodejs
-* npm install
 * sudo apt-get install sox
+* sudo apt-get install build-essential
+* npm install -g node-gyp
+* npm install
 * npm start
 * http://localhost:3000/?C
--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 /*jslint node: true, nomen: true, unparam: true, es5: true */
 
 'use strict';
-var express = require('express'), // server dep
-    shell = require('shelljs'),// run shell commands. fixed win->linux cmds.
-    url = require('url'),// url routing.
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http),
+	url = require('url'),
+	shell = require('shelljs'),
+	port = 3000;
 
-    port = 3000,
-    app = express(); // express object init. used to build the server.
+app.use('/static', express.static(__dirname + '/node_modules'));
 
-// get the default route.
 app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
+io.on('connection', function (socket){
+	console.log('a user connected');
+
+	socket.on('play', function (msg) {
+		shell.exec('play -qn synth 2 pluck ' + msg, {async: true});
+	})
+
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+	});
+});
+
+app.get('/api', function (req, res) {
     // '/?C will play a C note. ?Db plays a D flat note'
-    var note = url.parse(req.url).query; // do not regex to hell.
+    var note = url.parse(req.url).query;
 
-    // new shell process: do note playoff udp style.
-    shell.exec('play -qn synth 2 pluck ' + note, {async:true});
-
-    // http response: I god you covered!
+    shell.exec('play -qn synth 2 pluck ' + note, {async: true});
     res.status(200).send('Playing a note on the server!');
 });
 
-app.listen(port); // server init.
-console.log('Synth started at http://localhost:' + port + '/');
+http.listen(port, function () {
+	console.log('Synth started at http://localhost:' + port + '/');
+});
